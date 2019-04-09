ea9ed3d6f05c036570ebc48e1cbefe61fbdff02f
dhindsa91 <arvind.dhindsa1@gmail.com>
2019-04-08 18:06:04 -0700

    new java files

---
+++ jsprep/CallbackPromise/callback.js
@@ -0,0 +1,31 @@
+const posts = [
+    {body: "this is post one", title:"post one"}
+    ,
+    {body: "this is post two", title:"post two"}
+];
+
+
+function getPosts(){
+    setTimeout(()=>{
+        let output ='';
+        posts.forEach((post, index)=>{
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 2000);
+}
+
+function createPost(post, callback){
+
+setTimeout(()=> {
+
+        posts.push(post);
+        callback();
+},5000);
+
+
+}
+
+getPosts();
+
+createPost({title: "post three", body:"this is post 3"}, getPosts);
