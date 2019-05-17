44794ede6fc812ed62cb16f949c42b7e59dc036a
dhindsa91 <arvind.dhindsa1@gmail.com>
2019-05-16 20:30:06 -0700

    added new components

--- VueJS/vueapp/src/App.vue
+++ VueJS/vueapp/src/App.vue
@@ -1,17 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+   <Todos v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// export default, import in App, add to components
+
+
+
+import Todos from './components/Todos.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todos
+
+  }, data(){
+    return{
+      todos: 
+      [
+        {
+            id: 1,
+            title: "Todo One",
+            completed: false
+        },
+         {
+            id: 2,
+            title: "Todo Two",
+            completed: true
+        },
+         {
+            id: 3,
+            title: "Todo Three",
+            completed: false
+        }
+
+      ]
+    }
   }
 }
 </script>
---
+++ VueJS/vueapp/src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+
+<div class="todo-item">
+    <p>{{todo.title}}</p>
+</div>
+
+
+
+</template>
+
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"]
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
